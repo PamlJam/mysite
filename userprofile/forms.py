084df484ced919fe2f6d0ebca3ffa5285f83eee1
ubuntu <ubuntu@localhost.localdomain>
2023-01-16 20:18:36 +0800

    完善登录与注册

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,5 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import ValidationError
+
+def check_password(passwd):
+# 用于验证密码的有效性
+    hint = ''
+
+    if len(passwd) < 6:
+        hint = '密码长度至少为 6'
+         
+    if len(passwd) > 20:
+        hint = '密码长度至多为 20'
+         
+    if not any(char.isdigit() for char in passwd):
+        hint = '密码需要包含数字'
+         
+    if not any(char.isupper() for char in passwd):
+        hint =  '密码需要包含大写字母'
+         
+    if not any(char.islower() for char in passwd):
+        hint = '密码需要包含小写字母'
+
+    if not passwd.isalnum():
+        hint = '密码只能由数字与字母组成'
+
+    return hint
 
 class UserLoginForm(forms.Form):
     username = forms.CharField()
@@ -7,15 +32,39 @@ class UserLoginForm(forms.Form):
 
 class UserRegisterForm(forms.ModelForm):
     password = forms.CharField()
-    pwrepeat = forms.CharField()
+    repeat = forms.CharField()
 
     class Meta:
         model = User
         fields = ('username', 'email')
 
-    def clean_pwrepeat(self):
-        data = self.cleaned_data
-        if data.get('password') == data.get('pwrepeat'):
-            return data.get('password')
+    def clean_username(self):
+    # 覆写局部钩子
+        un = self.cleaned_data.get('username')
+        user = User.objects.filter(username = un).first()
+        if user:
+            raise ValidationError('用户已经存在')
         else:
-            raise forms.ValidationError("密码输入不一致，请重试。")
+            return un
+
+    def clean_password(self):
+    # 覆写局部钩子
+        pw = self.cleaned_data.get('password')
+        hint = check_password(pw)
+        if hint:
+            raise forms.ValidationError(hint)
+        else:
+            return pw
+
+    # def clean_repeat(self):
+    # 也可以在这验证，但是比较麻烦
+
+    def clean(self):
+    # 如果验证数据的时候，需要针对多个字段进行验证，那么最好覆写全局钩子
+        pw = self.cleaned_data.get('password')
+        re = self.cleaned_data.get('repeat')
+        if pw == re:
+            return pw
+        else:
+            return self.add_error('repeat','两次输入的密码不一致')
+            # 这样比直接在 clean_repeat 中 raise ValidationError 要好得多
--- userprofile/views.py
+++ userprofile/views.py
@@ -2,51 +2,54 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .forms import UserLoginForm, UserRegisterForm
+from django.views.decorators.http import require_http_methods
 
+
+@require_http_methods(["GET","POST"])  # 只允许这两种方法
 def user_register(request):
+    if request.user.is_authenticated:
+    # 只允许未登录的游客访问此视图
+        return redirect("/")
+
+    if request.method == 'GET':
+        return render(request, 'userprofile/register.html')
+
     if request.method == 'POST':
         form = UserRegisterForm(data = request.POST)
         if form.is_valid():
             new_user = form.save(commit = False)
             new_user.set_password(form.cleaned_data['password'])
-            # 一个经典错误：new_user.password = form.cleaned_data['password'] 
-            # 此时所设置密码是明文，无法进行认证 auth.authenticate()
             new_user.save()
             login(request, new_user)
             return redirect("/")
         else:
-            return HttpResponse("注册表单输入有误。请重新输入")
+            errorDict = form.errors
+    return render(request, 'userprofile/register.html', {"errorDict" : errorDict})
 
-    elif request.method == 'GET':
-    # 保留用户已经输入的内容
-        user_register_form = UserRegisterForm()
-        context = { 'form': user_register_form }
-        return render(request, 'userprofile/register.html', context)
-    else:
-        return HttpResponse("请使用 GET 或 POST 请求数据")
+@require_http_methods(["GET","POST"])  # 只允许这两种方法
+def user_login(request):
+    if request.user.is_authenticated:
+    # 只允许未登录的游客访问此视图
+        return redirect("/")
+
+    if request.method == 'GET':
+        return render(request, 'userprofile/login.html')
+
+    if request.method == 'POST':
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return redirect("/")
+            else:
+                form.add_error('password', "用户名或密码错误")
+                # 为 form.errors 字典增加错误信息
+
+        errorDict = form.errors        
+        return render(request, 'userprofile/login.html', {"errorDict" : errorDict})
 
 def user_logout(request):
     logout(request)
     return redirect("/")
-
-def user_login(request):
-    if request.method == 'POST':
-        user_login_form = UserLoginForm(data = request.POST)
-        if user_login_form.is_valid():
-            data = user_login_form.cleaned_data
-            user = authenticate(username=data['username'], password=data['password'])
-            if user:
-                login(request, user)
-                return HttpResponse("登录成功")
-            else:
-                return HttpResponse("账号或密码输入有误，请重新输入")
-        else:
-            return HttpResponse("账号或密码输入不合法")
-
-    elif request.method == 'GET':
-    # 保留用户已经输入的内容
-        user_login_form = UserLoginForm()
-        context = { 'form': user_login_form }
-        return render(request, 'userprofile/login.html', context)
-    else:
-        return HttpResponse("请使用 GET 或 POST 请求数据")
